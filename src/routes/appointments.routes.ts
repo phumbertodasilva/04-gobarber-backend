911e32864c6687a4ba7700c3eddb1b812660ed86
Paulo Humberto <phumbertodasilva@gmail.com>
2020-07-31 19:50:10 -0300

    - Corrigindo Server - Implementando Rota Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,4 +2,8 @@ import { Router } from 'express';
 
 const appointmentsRouter = Router();
 
+appointmentsRouter.post('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
+
 export default appointmentsRouter;
