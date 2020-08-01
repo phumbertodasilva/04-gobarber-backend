61df92d1671c9cff29cbdf6cd86fd32643b9c93a
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-01 11:40:21 -0300

    - Criando listar Todos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,8 +5,14 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
@@ -15,14 +21,14 @@ appointmentsRouter.post('/', (request, response) => {
   );
 
   if (findAppointmentsInSameDate) {
-    return response
+    return res
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
 
   const appointment = appointmentsRepository.create(provider, parsedDate);
 
-  return response.json(appointment);
+  return res.json(appointment);
 });
 
 export default appointmentsRouter;
