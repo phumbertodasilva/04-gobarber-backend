1af47eb3adef8f23bece40ebeb9844446288e188
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-01 11:35:12 -0300

    - Abstraindo Repository

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import Appointment from '../models/Appointment';
+import { isEqual } from 'date-fns';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointments = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointments || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
