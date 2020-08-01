5f485efd3e6ea98b72c0c87ea0a61375450c23e3
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-01 10:55:39 -0300

    - Corrigido acesso a rota

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.post('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
