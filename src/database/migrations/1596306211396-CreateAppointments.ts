e4bc20ba45ee3ca17365b0bd864a7e454d725f7a
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-01 15:59:43 -0300

    - Implementado TypeOrm no Service, Repository, Rotes - Corrigindo Migration - Implementando lib necessaria

--- src/database/migrations/1596306211396-CreateAppointments.ts
+++ src/database/migrations/1596306211396-CreateAppointments.ts
@@ -11,6 +11,7 @@ export class CreateAppointments1596306211396 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
