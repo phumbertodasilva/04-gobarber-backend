f80c2c48851e6220a8374e5b1571c88a7efc21cc
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-03 19:34:23 -0300

    - Atualizando o Avatar e finalizando o serviço

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -29,7 +31,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (req, res) => {
-    return res.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      delete user.password;
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
   },
 );
 
