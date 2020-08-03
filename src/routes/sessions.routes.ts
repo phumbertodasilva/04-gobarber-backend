fedd35d70bbd82465e6c43dcb7ce4932e68c1ff4
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-03 19:54:36 -0300

    - Tratativa de erros globais

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,19 +5,15 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return res.json({ user, token });
-  } catch (e) {
-    return res.status(400).json({ error: e.message });
-  }
+  return res.json({ user, token });
 });
 
 export default sessionsRouter;
