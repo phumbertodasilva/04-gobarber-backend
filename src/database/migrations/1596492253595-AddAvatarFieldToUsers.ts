feaf2d3b79ea4768ccbb1b7a88422c30f552e88e
Paulo Humberto <phumbertodasilva@gmail.com>
2020-08-03 19:21:54 -0300

    - Implementando upload

---
+++ src/database/migrations/1596492253595-AddAvatarFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { TableColumn } from 'typeorm/index';
+
+export class AddAvatarFieldToUsers1596492253595 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
